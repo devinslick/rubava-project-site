e2a2e687658d04df6dc72a36c0c4012f28bd7ec4
Nelson Blaha <nelson.blaha@gmail.com>
2012-05-07 09:58:12 -0500

    A little more gravitar stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,4 @@
 module ApplicationHelper
 require 'digest/md5'
+def gravatar_url_for(email, options = {})      url_for({ :gravatar_id => Digest::MD5.hexdigest(email), :host => 'www.gravatar.com',                     :protocol => 'http://', :only_path => false, :controller => 'avatar.php'                }.merge(options))  end
 end
