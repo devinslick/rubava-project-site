a274e782d8d80f1d828c0561c37613be462781fe
nelson <nelson@ringo.(none)>
2012-05-17 19:41:14 -0500

    made a bullshit comment in routes.rb to demonstrate changes to tabi,devin,mashallah

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# some bullshit comment just so things will be different
+
 Rubava::Application.routes.draw do
   get "log_in" => "sessions#new", :as => "log_in"
   get "log_out" => "sessions#destroy", :as => "log_out"  
